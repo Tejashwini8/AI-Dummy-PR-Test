7c6000f6c248749ae9f43500d55ef2894b9f2920
Tejashwini8 <69107966+Tejashwini8@users.noreply.github.com>
2025-11-07 18:37:47 +0530

    Update dummy_code.py

--- dummy_code.py
+++ dummy_code.py
@@ -1,7 +1,4 @@
 # inefficient code example
-data = [i for i in range(10000)]
-result = []
-for i in data:
-    if i % 2 == 0:
-        result.append(i * 2)
+data = range(10000)
+result = [i * 2 for i in data if i % 2 == 0]
 print(sum(result))
